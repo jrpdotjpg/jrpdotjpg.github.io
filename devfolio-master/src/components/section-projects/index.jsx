3828808df0e3806f9fe135cc2122f7c065d76772
Jonathan Paynter <jrp2018@ic.ac.uk>
2021-07-03 15:38:42 +0100

    Reverting section-projects component

--- devfolio-master/src/components/section-projects/index.jsx
+++ devfolio-master/src/components/section-projects/index.jsx
@@ -1,45 +1,69 @@
-import { Link } from 'gatsby';
-import React from 'react';
+// import { Link } from 'gatsby';
+// import React from 'react';
 
-import Section from '../section';
-import SummaryItem from '../summary-item';
+// import Section from '../section';
+// import SummaryItem from '../summary-item';
+
+// // const SectionProjects = ({ projects }) => {
+// //   if (!projects.length) return null;
+
+// //   return (
+// //     <Section title="Projects">
+// //       {projects.map((project) => (
+// //         <SummaryItem
+// //           key={project.name}
+// //           name={project.name}
+// //           description={project.description}
+// //           link={project.link}
+// //         />
+// //       ))}
+// //     </Section>
+// //   );
+// // };
+
+// // export default SectionProjects;
 
 // const SectionProjects = ({ projects }) => {
-//   if (!projects.length) return null;
-
 //   return (
-//     <Section title="Projects">
+//     <Section title="In more detail...">
 //       {projects.map((project) => (
 //         <SummaryItem
-//           key={project.name}
-//           name={project.name}
-//           description={project.description}
-//           link={project.link}
+//           key={project.node.fields.slug}
+//           name={project.node.frontmatter.title}
+//           description={project.node.frontmatter.description}
+//           link={project.node.fields.slug}
+//           internal
 //         />
 //       ))}
+//       {projects.length >= 5 && (
+//         <Link className="text-gray-500 text-sm hover:text-black" to="/blog">
+//           View projects &rarr;
+//         </Link>
+//       )}
 //     </Section>
 //   );
 // };
 
 // export default SectionProjects;
 
+import React from 'react';
+
+import Section from '../section';
+import SummaryItem from '../summary-item';
+
 const SectionProjects = ({ projects }) => {
+  if (!projects.length) return null;
+
   return (
-    <Section title="In more detail...">
+    <Section title="Projects">
       {projects.map((project) => (
         <SummaryItem
-          key={project.node.fields.slug}
-          name={project.node.frontmatter.title}
-          description={project.node.frontmatter.description}
-          link={project.node.fields.slug}
-          internal
+          key={project.name}
+          name={project.name}
+          description={project.description}
+          link={project.link}
         />
       ))}
-      {projects.length >= 5 && (
-        <Link className="text-gray-500 text-sm hover:text-black" to="/blog">
-          View projects &rarr;
-        </Link>
-      )}
     </Section>
   );
 };
